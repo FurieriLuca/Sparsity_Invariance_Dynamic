560f9b9589e09c4a4dde5c31594d304e15e10136
Luca Furieri <furieril@control.ee.ethz.ch>
2018-10-19 18:15:55 +0200

    Added simple example where block-diagonal infeasible, but sparsity invariance feasible

---
+++ Static/Example simple/StrucH2LMI_new.m
@@ -0,0 +1,54 @@
+function [K,J,X1] = StrucH2LMI_new(A,B1,B2,Q,R,T,Rstruct)
+% Structured Optimal control over directed graphs: SDP relaxation via block
+% diagnal Lyapunov function
+% Input data: graph Gp, Gc; Dynamic matrices: A, B1, B2, --> cell format
+%             Performance Index: Q1,Q2 --> penalize on state, Q1: absolute, Q2: relative
+% Outpute data: Jdiag, performance, K, conresponding controller
+
+epsilon = 0.000001;
+
+n=size(A,1);
+m=size(B2,2);
+%% solution via Yalmip
+% variables
+X = sdpvar(n,n);        %% Matrix Z has sparsity pattern in Gc
+for i = 1:n                     
+    for j = 1:n
+        if Rstruct(i,j)==0
+             X(i,j) = 0;
+        end
+    end
+end
+
+Z = sdpvar(m,n);        %% Matrix Z has sparsity pattern in Gc
+for i = 1:m                     
+    for j = 1:n
+        if T(i,j)==0
+             Z(i,j) = 0;
+        end
+    end
+end
+
+% constraint
+Y = sdpvar(m);
+Const = [X-epsilon*eye(n) >=0, ...
+    (A*X-B2*Z)+(A*X-B2*Z)'+B1*B1' + epsilon* eye(n) <= 0,...
+    [Y Z; Z' X]>=0];
+
+% cost function
+Obj = trace(Q*X)+trace(R*Y);
+
+ops = sdpsettings('solver','sedumi');
+Info = optimize(Const,Obj,ops);
+% solution
+
+X1 = value(X);Z1 = value(Z);Y1 = value(Y);
+
+K = Z1*X1^(-1);  % controller
+
+%% H2 performance using Lyapunov equation
+P = lyap((A-B2*K)',Q+K'*R*K);
+J = sqrt(trace(P*(B1*B1')));
+
+end
+
--- Static/plots.m
+++ Static/plots.m
@@ -1,4 +1,4 @@
-leaders_vec=0:1:L;
+lead=0:1:L;
 
 figure(1)
 hold on;
@@ -12,15 +12,15 @@ for(i=1:size(x_columns,1)/2)
 end
 %}
 
-plot(leaders_vec(1,:),J(:,1),'-kv','LineWidth',3,'MarkerSize',15);
-plot(leaders_vec(1,:),J(:,2),'--ks','LineWidth',3,'MarkerSize',15);
-plot(leaders_vec(1,:),J(:,3),'-bo','LineWidth',4,'MarkerSize',15);
-plot(leaders_vec(1,:),J(:,4),'--bd','LineWidth',3,'MarkerSize',15);
+plot(lead(1,:),J(:,1),'-kv','LineWidth',3,'MarkerSize',15);
+%plot(leaders_vec(1,:),J(:,2),'--ks','LineWidth',3,'MarkerSize',15);
+plot(lead(1,:),J(:,3),'-bo','LineWidth',4,'MarkerSize',15);
+%plot(leaders_vec(1,:),J(:,4),'--bd','LineWidth',3,'MarkerSize',15);
 
-plot(leaders_vec(1,:),J(:,5),'-gx','LineWidth',3,'MarkerSize',15);
-plot(leaders_vec(1,:),J(:,6),'--g*','LineWidth',3,'MarkerSize',15);
+plot(lead(1,:),J(:,5),'-gx','LineWidth',3,'MarkerSize',15);
+%plot(leaders_vec(1,:),J(:,6),'--g*','LineWidth',3,'MarkerSize',15);
 
-plot(leaders_vec(1,:),J(:,7),'--r','LineWidth',3,'MarkerSize',15);
+plot(lead(1,:),J(:,7),'--r','LineWidth',3,'MarkerSize',15);
 %plot(leaders_vec(1,:),J(:,6),'--g','LineWidth',2);
 
 set(gca,'fontsize',30,'fontweight','bold')
@@ -29,6 +29,7 @@ ylabel('H2 norm','fontweight','bold','fontsize',30);
 
 
 %legend('block-diagonal','GD from block-diagonal','spars. inv. with T = S', 'GD from spars. inv. with T=S', 'spars. inv. with T = max. cliques','GD from spars. inv. with T = max. cliques','centralized')
+legend('block-diagonal','spars. inv. with T = S', 'spars. inv. with T = T_{new}','centralized')
 
 %plot(leaders_vec(1,:),J(:,1),'bx','LineWidth',2);
 
@@ -45,6 +46,6 @@ plot(leaders_vec(1,:),degrees(:,2),'-gx','LineWidth',3,'MarkerSize',15);
 set(gca,'fontsize',30,'fontweight','bold')
 xlabel('L','fontweight','bold','fontsize',30);
 ylabel('separation','fontweight','bold','fontsize',30)
-
+legend('spars. inv. with T = S','spars. inv. with T = max. cliques');
 
 
